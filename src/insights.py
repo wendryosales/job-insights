4a7421859efdc3d1413e17542b67dabff96a3cf5
Wendryo <wendryo.sales@gmail.com>
2022-10-24 12:55:25 -0300

    feat: :sparkles: Function get_unique_industries

--- src/insights.py
+++ src/insights.py
@@ -53,7 +53,12 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    original_data = read(path)
+    uniques_data = set()
+    for job in original_data:
+        if job["industry"]:
+            uniques_data.add(job["industry"])
+    return uniques_data
 
 
 def filter_by_industry(jobs, industry):
