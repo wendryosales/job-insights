c326eb5bd47a404767d9694ef219c2a43c775aa2
Wendryo <wendryo.sales@gmail.com>
2022-10-24 12:11:56 -0300

    feat: :sparkles: Function Read

--- src/jobs.py
+++ src/jobs.py
@@ -1,4 +1,5 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
@@ -15,4 +16,6 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        return list(reader)
