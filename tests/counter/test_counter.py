d24f6da09369c9a13c4424d1581c2b99e947b312
Wendryo <wendryo.sales@gmail.com>
2022-10-24 14:46:18 -0300

    test: :test_tube: Test_counter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "src/jobs.csv"
+    python_results = count_ocurrences(path, "Python")
+    javascript_results = count_ocurrences(path, "Javascript")
+    assert python_results == 1639
+    assert javascript_results == 122
